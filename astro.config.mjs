e4c4b279ffb15faec7a190d6f5a93c6c1b182009
pwryu <parinphat.wasinchaikulpat@qantas.com.au>
2023-02-18 22:18:50 +0700

    finished!

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [preact()]
 });
